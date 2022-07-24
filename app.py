6e9b0b6e5333fa92e39483e864e805a53ab385b3
sriram-kashyap <109916424+sriram-kashyap@users.noreply.github.com>
2022-07-24 22:58:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,10 @@ st.write(
 )
 st.header("User Input Parameters")
 
+st.write("Enter the values for the two numbers and press Enter to apply")
 
 num1 = st.number_input("NUM1", min_value = -9999999999, max_value=9999999999, key="num1")
 num2 = st.number_input("NUM1", min_value = -9999999999, max_value=9999999999, key="num2")
 answer = num1+num2
 
-st.write(answer)
+st.write("{} + {} = {}".format(num1, num2, answer))
