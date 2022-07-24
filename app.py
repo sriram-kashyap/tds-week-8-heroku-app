9b37c36d5076f543cb5ecf744a9dfa971832e94d
sriram-kashyap <109916424+sriram-kashyap@users.noreply.github.com>
2022-07-24 22:46:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,19 +6,9 @@ st.write(
 )
 st.header("User Input Parameters")
 
-def user_input_parameters():
-  num1 = st.number_input("NUM1", min_value = -9999999999, max_value=9999999999, key="num1")
-  num2 = st.number_input("NUM1", min_value = -9999999999, max_value=9999999999, key="num2")
 
-  data = {
-    "NUM1" : num1,
-    "NUM2" : num2
-  }
-
-  return num1, num2
-
-num1, num2 = user_input_parameters()
-
+num1 = st.number_input("NUM1", min_value = -9999999999, max_value=9999999999, key="num1")
+num2 = st.number_input("NUM1", min_value = -9999999999, max_value=9999999999, key="num2")
 answer = num1+num2
 
 st.write(answer)
